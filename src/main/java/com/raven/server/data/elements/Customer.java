89b63d59542abd4984d398026b1b4f11631126c8
MrGamer2k17 <yar300102@gmail.com>
2021-08-05 00:39:31 +0300

    use lombok

--- src/main/java/com/raven/server/data/elements/Customer.java
+++ src/main/java/com/raven/server/data/elements/Customer.java
@@ -1,19 +1,23 @@
 package com.raven.server.data.elements;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.*;
 
 import javax.persistence.*;
-import java.time.Instant;
-import java.util.Objects;
 
 @Entity
 @Table
+@Getter
+@Setter
+@ToString(exclude = {"created", "updated", "isActive"})
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
 public class Customer {
     @Id
     @Column(updatable = false)
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonView(Views.MainView.class)
-    private Long id;
+    private long id;
 
     @JsonView(Views.MainView.class)
     private String fullName;
@@ -25,97 +29,9 @@ public class Customer {
     private String phone;
 
     @Column(updatable = false)
-    private Long created;
+    private long created;
 
-    private Long updated;
+    private long updated;
 
-    private Boolean isActive;
-
-    public Customer() {}
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCreated(Long created) {
-        this.created = created;
-    }
-
-    public void setUpdated(Long updated) {
-        this.updated = updated;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getCreated() {
-        return created;
-    }
-
-    public Long getUpdated() {
-        return updated;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Customer customer = (Customer) o;
-        return id.equals(customer.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", created=" + created +
-                ", updated=" + updated +
-                ", fullName='" + fullName + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", isActive=" + isActive +
-                '}';
-    }
+    private boolean isActive;
 }
